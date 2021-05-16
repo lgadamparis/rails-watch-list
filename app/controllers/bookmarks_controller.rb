e6963d081cb428bc82df287e6c6f2ebaf621efc3
louis gonzague adam <lg.adam@mac.com>
2021-05-16 17:02:23 +0200

    enhance wish list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,11 +4,13 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    @list = List.find(params[:list_id])
+    @bookmark.list_id = @list.id
     if @bookmark.save
       redirect_to list_path(@list)
     else
